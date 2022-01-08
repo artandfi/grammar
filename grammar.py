69ac67f2c404dc1df4e7ef05d67871af0b504a4b
Artyom Fisunenko <artyom.fisunenko@gmail.com>
2022-01-08 19:55:06 +0200

    update constants.py

--- grammar.py
+++ grammar.py
@@ -5,11 +5,16 @@ from constants import AXIOM, EPS, TERMINAL_PATTERN, NONTERMINAL_PATTERN
 from rule import Rule
 
 
+ERROR_NO_RULES = 'Rules not specified'
 ERROR_NO_AXIOM = f'Grammar must contain at least one rule starting with axiom {AXIOM}'
-ERROR_NO_RULES = f'Rules not specified'
-
+ERROR_RULES_TYPE_0 = 'Rules must be of form aAb -> ayb, where A is a nonterminal, a and b are words, and y is a non-empty word.'
+ERROR_RULES_TYPE_1 = 'Rules must be of form '
 
 class Grammar:
+    """
+        Type-0 (recursively enumerable) grammar class.
+        Does not put any constraints on its rules.
+    """
     def __init__(self, rules):
         if not rules:
             raise ValueError(ERROR_NO_RULES)
@@ -29,6 +34,15 @@ class Grammar:
         ] + [f'{i+1}) {rule}' for i, rule in enumerate(self.rules)])
 
 
-rules = ['S -> Aa', 'A -> bc', 'S -> ABCaA', 'B -> Ca', 'C -> _']
-g = Grammar([Rule(r) for r in rules])
-print(g)
+
+class ContextSensitiveGrammar(Grammar):
+    """
+        Type-1 (context-sensitive) grammar.
+        Rules must be of form aAb -> ayb, where A is a nonterminal, a and b are words,
+        and y is a non-empty word.
+    """
+
+    def __init__(self, rules):
+        super().__init__(rules)
+
+        if 
--- rule.py
+++ rule.py
@@ -1,5 +1,5 @@
 import re
-from constants import RULE_REGEX, RULE_ARROW
+from constants import RULE_PATTERN_TYPE_0, ARROW
 
 
 ERROR_RULE_INCORRECT = 'Rule must be in form p -> q where p and q are words, and p has at least one nonterminal character.'
@@ -7,14 +7,14 @@ ERROR_RULE_INCORRECT = 'Rule must be in form p -> q where p and q are words, and
 
 class Rule:
     def __init__(self, rule):
-        if not re.match(RULE_REGEX, rule):
+        if not re.match(RULE_PATTERN_TYPE_0, rule):
             raise ValueError(ERROR_RULE_INCORRECT)
         
         rule = re.sub('\\s*', '', rule)
-        self.lhs, self.rhs = rule.split(RULE_ARROW)
+        self.lhs, self.rhs = rule.split(ARROW)
     
     def __str__(self):
-        return f'{self.lhs} {RULE_ARROW} {self.rhs}'
+        return f'{self.lhs} {ARROW} {self.rhs}'
 
     def __repr__(self):
         return f'{self.__class__.__name__}(\'{self.__str__()}\')'
