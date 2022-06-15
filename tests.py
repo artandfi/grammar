a0e7ecafd578807615c19474a75470c16e2f8378
Artyom Fisunenko <artyom.fisunenko@gmail.com>
2022-06-15 16:17:26 +0300

    extend tests

--- tests.py
+++ tests.py
@@ -8,16 +8,23 @@ class TestGrammars(TestCase):
         rules = [
             Rule("S->a"), Rule("S->A"), Rule("A->AB"), Rule("B->b"), Rule("C->c"), Rule("E->Ff")]
         new_rules = rules[:-2]
+        new_terminals = {'_', 'a', 'b'}
+        new_nonterminals = {'S', 'A', 'B'}
         
         grammar = ContextFreeGrammar(rules)
         grammar.remove_unreachable()
 
         self.assertEqual(grammar.rules, new_rules) # Last two rules should be unreachable
+        self.assertEqual(grammar.terminals, new_terminals)
+        self.assertEqual(grammar.nonterminals, new_nonterminals)
     
     def test_remove_unreachable2(self):
         rules = [Rule("S->BbB"), Rule("S->AaA"), Rule("B->ABC"), Rule("C->Dc"), Rule("D->_")]
-        
+
         grammar = ContextFreeGrammar(rules)
+        new_terminals, new_nonterminals = grammar.terminals, grammar.nonterminals
         grammar.remove_unreachable()
 
         self.assertEqual(grammar.rules, rules) # All rules should be reachable
+        self.assertEqual(grammar.terminals, new_terminals)
+        self.assertEqual(grammar.nonterminals, new_nonterminals)
